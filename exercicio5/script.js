83ad40140d8f13301d82e7a9429cc256669fb6ac
Lorenzo Vanlare Sprenger <lorenzosprenger@gmail.com>
2024-03-12 11:14:25 -0300

    upando

---
+++ exercicio5/script.js
@@ -0,0 +1,12 @@
+const vermelho = document.getElementById("vermelho");
+const verde = document.getElementById("verde");
+
+vermelho.addEventListener('click', function(){
+  document.body.style.backgroundColor = 'red';
+
+});
+
+verde.addEventListener('click', function(){
+  document.body.style.backgroundColor = 'green';
+
+});
